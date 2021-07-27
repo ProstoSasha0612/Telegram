38b2dd357b6db41e539defeaa23c8799a6998eaf
Alexandr Popko <alexandr.popko.2013@mail.ru>
2021-07-27 19:09:30 +0300

    init: create project

---
+++ app/src/main/java/com/hfad/android/telegram/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hfad.android.telegram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
